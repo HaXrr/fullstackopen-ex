a0ba240c03b01c275785126b163bfe226a95c77a
HaXrr <hazratabubakar91@gmail.com>
2025-09-07 09:16:42 -0700

    Completed exercise 1.3: refactored Courseinfo app to use objects

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,26 +1,27 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
       <Header name={course} />
       <Content 
-      exercises1={exercises1} 
-      exercises2={exercises2}
-      exercises3={exercises3}
       part1={part1}
       part2={part2}
       part3={part3}
       />
       
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
      
     </div>
   )
@@ -40,10 +41,13 @@ const Content = (props)=>{
     return(
       <div>
       <p>
-        {props.part1} {props.exercises1}
+        {props.part1.name} {props.part1.exercises}
       </p>
       <p>
-        {props.part2} {props.exercises2}
+        {props.part2.name} {props.part2.exercises}
+      </p>
+      <p>
+        {props.part3.name} {props.part3.exercises}
       </p>
       </div>
     )
