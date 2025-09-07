31a83e3010af2b58a51d5e9ddde38eebcdb2a83b
HaXrr <hazratabubakar91@gmail.com>
2025-09-07 09:30:34 -0700

    Completed exercise 1.5: refactored Courseinfo app to use a single course object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,28 +1,31 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+    const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
     <div>
-      <Header name={course} />
+      <Header name={course.name} />
       <Content 
-      parts={parts}
+      course={course}
       
       />
       
-      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+      <Total total={ course.parts[0].exercises + course.parts[1].exercises+ course.parts[2].exercises}/>
      
     </div>
   )
@@ -42,13 +45,13 @@ const Content = (props)=>{
     return(
       <div>
       <p>
-        {props.parts[0].name} {props.parts[0].exercises}
+        {props.course.parts[0].name} {props.course.parts[0].exercises}
       </p>
       <p>
-        {props.parts[1].name} {props.parts[1].exercises}
+        {props.course.parts[1].name} {props.course.parts[1].exercises}
       </p>
       <p>
-        {props.parts[2].name} {props.parts[2].exercises}
+        {props.course.parts[2].name} {props.course.parts[2].exercises}
       </p>
       </div>
     )
