509d82070eec71e32a80486856b28ec8007d9d9d
HaXrr <hazratabubakar91@gmail.com>
2025-09-08 21:51:25 -0700

    1.10 unicafe step 5: Extract Button and StatisticLine components

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,43 +1,50 @@
 import { useState } from 'react'
 
 
-const Statistics = ( { good, bad, neutral } ) => {
-
-  const total = good + bad + neutral
-  const average = (good*1 + neutral*0 + bad*(-1))/total
-  const positive = ((good/total)*100)
-
-  return(
-      <div>
-        <h1>Statistic</h1>
-        <p>Good: {good}</p>
-        <p>Bad: {bad} </p>
-        <p>Neutral: {neutral}</p>
-
-        {
-          total === 0 ? <p>No feedback yet</p> : <>
-            <p>Average: {average.toFixed(2)}</p>
-            <p>Positive: {positive.toFixed(2)}%</p></>
-        }
-      </div>
+const StatisticLine = ({ state, text }) => {
+  return (
+    <p>{text}:{state}</p>
   )
 }
 
-const Button = ( {handleClick, text} ) => {
-  return(
+const Statistics = ({ good, bad, neutral }) => {
+
+  const total = good + bad + neutral
+  const average = (good * 1 + neutral * 0 + bad * (-1)) / total
+  const positive = ((good / total) * 100)
+
+  return (
+    <div>
+      <h1>Statistic</h1>
+      <StatisticLine state={good} text={"Good"} />
+      <StatisticLine state={bad} text={"Bad"} />
+      <StatisticLine state={neutral} text={"Neutral"} />
+
+      {
+        total === 0 ? <p>No feedback yet</p> : <>
+          <StatisticLine state={average} text={"Average"} />
+          <StatisticLine state={positive} text={"Positive"} />
+        </>
+      }
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
     <button onClick={handleClick}>{text}</button>
 
   )
 }
 
-const Controls = ({handleGood, handleNeutral, handleBad}) => {
-  return(
+const Controls = ({ handleGood, handleNeutral, handleBad }) => {
+  return (
     <div>
       <h1>GIve FeedBack</h1>
 
-      <Button handleClick={handleGood} text={"Good"}/>
-      <Button handleClick={handleNeutral} text={"Neutral"}/>
-      <Button handleClick={handleBad} text={"Bad"}/>
+      <Button handleClick={handleGood} text={"Good"} />
+      <Button handleClick={handleNeutral} text={"Neutral"} />
+      <Button handleClick={handleBad} text={"Bad"} />
     </div>
   )
 }
@@ -48,23 +55,23 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
+
 
   const handleGood = () => {
-    setGood(good+1)
+    setGood(good + 1)
   }
-   const handleNeutral = () => {
-    setNeutral(neutral+1)
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
   }
-   const handleBad = () => {
-    setBad(bad+1)
+  const handleBad = () => {
+    setBad(bad + 1)
   }
 
- 
+
   return (
     <div>
 
-      <Controls handleGood={handleGood} handleNeutral={handleNeutral} handleBad={handleBad}   />
+      <Controls handleGood={handleGood} handleNeutral={handleNeutral} handleBad={handleBad} />
       <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
