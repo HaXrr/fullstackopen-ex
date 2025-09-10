99df3aabf4e3d54c6ae3a4d61d52077807e739e5
HaXrr <hazratabubakar91@gmail.com>
2025-09-10 03:13:55 -0700

    ex:2.9 implemented search functionality on the name of persons

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [phone, setPhone] = useState("")
+  const [search, setSearch]= useState("")
   const addPerson = (event) => {
     event.preventDefault()
     const randId = Math.random()*persons.length
 
     if(!persons.some(person=> person.name ===newName)){
-      setPersons(persons.concat({name: newName, phone: phone, id: randId}))
+      setPersons(persons.concat({name: newName, number: phone, id: randId}))
 
     }
     else{
@@ -33,6 +37,14 @@ const App = () => {
     setPhone(e.target.value)
   }
 
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+    console.log("searching", search)
+  }
+  const personsToShow = persons.filter(person =>
+  person.name.toLowerCase().includes(search.toLowerCase())
+)
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -47,13 +59,19 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+      <br />
+      <div>
+        Search <input type="text" placeholder='search here by name' onChange={handleSearch} />
+      </div>
       <h2>Numbers</h2>
-      {
-        persons.map((person,i)=>{
-          return <p key={i} >name: {person.name}  _____  phone: {person.phone}</p>
+      <div>
+        {
+        personsToShow.map((person,i)=>{
+          return <p key={i} >name: {person.name}  _____  phone: {person.number}</p>
              
         })
       }
+      </div>
       ...
     </div>
   )
