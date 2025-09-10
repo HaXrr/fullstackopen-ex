a3d701c1c3f4d40db92aed1729fe0e26d72fae29
HaXrr <hazratabubakar91@gmail.com>
2025-09-10 01:25:26 -0700

    exercies: 2.4 done, rendering multiple courses

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,27 +1,52 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return courses.map((course)=>{
+    return <Course course={course} key={course.id} />
+  })
 }
 
 const Course = ({ course }) => {
@@ -41,19 +66,19 @@ const Content = ({ parts }) => {
   console.log(parts)
   // const total = parts[0].exercises+parts[1].exercises+parts[2].exercises
   // console.log("sum of exercises are: ", total)
-  const total = parts.reduce((s,p)=>{
-    console.log('what is happening', s,"p= ", p)
-    return s+p.exercises
-  },0)
+  const total = parts.reduce((s, p) => {
+    console.log('what is happening', s, "p= ", p)
+    return s + p.exercises
+  }, 0)
 
   return (
     <div>
       <div>
         {
-        parts.map((part) => {
-          return <Part key={part.id} part={part} />
-        })
-      }
+          parts.map((part) => {
+            return <Part key={part.id} part={part} />
+          })
+        }
       </div>
       <p>Total Exersice: {total}</p>
     </div>
