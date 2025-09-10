408b3919cb8b5b417b38895658b1147e5686fe7c
HaXrr <hazratabubakar91@gmail.com>
2025-09-10 02:38:16 -0700

    ex:2.8 added phone functionality

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,27 +2,35 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    
   ]) 
   const [newName, setNewName] = useState('')
-
+  const [phone, setPhone] = useState("")
   const addPerson = (event) => {
     event.preventDefault()
+    const randId = Math.random()*persons.length
+
     if(!persons.some(person=> person.name ===newName)){
-      setPersons(persons.concat({name: newName}))
+      setPersons(persons.concat({name: newName, phone: phone, id: randId}))
+
     }
     else{
       alert(`${newName} Already exist`)
       }
     
     setNewName("")
-
+    setPhone("")
     console.log("Form Submited", persons)
   }
 
   const handlePersonChange = (e) => {
     console.log("New person", e.target.value)
     setNewName(e.target.value)
+  }
+
+  const handlePhoneChange = (e)=>{
+    console.log(e.target.value)
+    setPhone(e.target.value)
   }
 
   return (
@@ -33,13 +41,17 @@ const App = () => {
           name: <input value={newName} onChange={handlePersonChange} />
         </div>
         <div>
+          Phone: <input type='phone' value={phone} onChange={handlePhoneChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {
         persons.map((person,i)=>{
-          return <p key={i} >{person.name}</p>
+          return <p key={i} >name: {person.name}  _____  phone: {person.phone}</p>
+             
         })
       }
       ...
