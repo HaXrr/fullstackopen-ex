51be5b6f8ba4334c3fb95ed0d7dd1be002535a77
HaXrr <hazratabubakar91@gmail.com>
2025-09-10 01:29:00 -0700

    exercise: 2.5 separation of course module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from "./Course"
+
 const App = () => {
   const courses = [
     {
@@ -44,63 +46,10 @@ const App = () => {
     }
   ]
 
-  return courses.map((course)=>{
+  return courses.map((course) => {
     return <Course course={course} key={course.id} />
   })
 }
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-    </>
-  )
-}
-
-const Header = ({ name }) => {
-  return <h1>{name}</h1>
-}
-
-const Content = ({ parts }) => {
-  console.log(parts)
-  // const total = parts[0].exercises+parts[1].exercises+parts[2].exercises
-  // console.log("sum of exercises are: ", total)
-  const total = parts.reduce((s, p) => {
-    console.log('what is happening', s, "p= ", p)
-    return s + p.exercises
-  }, 0)
-
-  return (
-    <div>
-      <div>
-        {
-          parts.map((part) => {
-            return <Part key={part.id} part={part} />
-          })
-        }
-      </div>
-      <p>Total Exersice: {total}</p>
-    </div>
-  )
-}
-
-
-const Part = ({ part }) => {
-  console.log("single part", part)
-  return <p key={part.id}>{part.name} ex: {part.exercises}</p>
-
-}
-
-
-
-
-
-
-
-
-
-
 export default App
 
 
