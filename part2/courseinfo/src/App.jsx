cddf108cdbe7536fd59344859d95952d1041d613
HaXrr <hazratabubakar91@gmail.com>
2025-09-09 22:50:24 -0700

    feat(part2): add Course component structure with Header, Content, and Part (exercise 2.1)

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,62 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>
+}
+
+const Content = ({ parts }) => {
+  console.log(parts)
+  return (
+    <div>
+      {
+        parts.map((part) => {
+          return <Part key={part.id} part={part} />
+        })
+      }
+    </div>
+  )
+}
+
+
+const Part = ({ part }) => {
+  console.log("single part", part)
+  return <p key={part.id}>{part.name} ex: {part.exercises}</p>
+
+}
+
+export default App
+
+
