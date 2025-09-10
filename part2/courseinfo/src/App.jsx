a46f4e38272779b885984911944d5d6ba5352eb9
HaXrr <hazratabubakar91@gmail.com>
2025-09-10 01:21:58 -0700

    exercise: 2.3 summ of exercies with arrays reduce() method

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -39,9 +39,12 @@ const Header = ({ name }) => {
 
 const Content = ({ parts }) => {
   console.log(parts)
-  const total = parts[0].exercises+parts[1].exercises+parts[2].exercises
-  console.log("sum of exercises are: ", total)
-
+  // const total = parts[0].exercises+parts[1].exercises+parts[2].exercises
+  // console.log("sum of exercises are: ", total)
+  const total = parts.reduce((s,p)=>{
+    console.log('what is happening', s,"p= ", p)
+    return s+p.exercises
+  },0)
 
   return (
     <div>
