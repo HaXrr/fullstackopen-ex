297391023e5e17450489d9f4f10184b04bb6c802
HaXrr <hazratabubakar91@gmail.com>
2025-09-10 23:43:55 -0700

    ex2.11 fetched data from server with useEffect Hook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,25 +3,21 @@ import Form from './Form'
 import Filter from './Filter'
 import Persons from './Persons'
 import axios from 'axios'
-
-
-
-
-
-
-
-
-
-
-
+import { useEffect } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
+  useEffect(()=>{
+    axios.get('http://localhost:3001/persons')
+    .then((res)=>{
+      console.log(res.data)
+      setPersons(res.data)
+
+    })
+  },[])
+  
   const [newName, setNewName] = useState('')
   const [phone, setPhone] = useState("")
   const [search, setSearch] = useState("")
