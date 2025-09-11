f9121ee9bd55f597970953fee2483a799b64ffdb
HaXrr <hazratabubakar91@gmail.com>
2025-09-11 00:27:21 -0700

    ex:2.13 refactored the backedn into module in separate file /services/personService.js

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,85 +1,59 @@
-import { useState } from 'react'
-import Form from './Form'
-import Filter from './Filter'
-import Persons from './Persons'
-import axios from 'axios'
-import { useEffect } from 'react'
+import { useState, useEffect } from "react";
+import Form from "./Form";
+import Filter from "./Filter";
+import Persons from "./Persons";
+import personService from "./services/personService"; // <-- new import
 
 const App = () => {
-  const [persons, setPersons] = useState([
-  ])
- 
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [search, setSearch] = useState("");
 
-  useEffect(()=>{
-    axios.get('http://localhost:3001/persons')
-    .then((res)=>{
-      console.log(res.data)
-      setPersons(res.data)
-    
-    })
-  },[])
-
-  const [newName, setNewName] = useState('')
-  const [phone, setPhone] = useState("")
-  const [search, setSearch] = useState("")
+  useEffect(() => {
+    personService.getAll().then(initialPersons => {
+      setPersons(initialPersons);
+    });
+  }, []);
 
   const addPerson = (event) => {
-    event.preventDefault()
-    
+    event.preventDefault();
 
     if (!persons.some(person => person.name === newName)) {
-      
-      axios.post('http://localhost:3001/persons', {name: newName, number:phone})
-      .then((res)=>{
-        console.log(res)
-        setPersons(persons.concat(res.data))
-        setNewName("")
-    setPhone("")
+      const personObject = { name: newName, number: phone };
 
-      })
-      .catch((err)=>{
-        console.log("error adding person data", err)
-      })
+      personService.create(personObject).then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson));
+        setNewName("");
+        setPhone("");
+      });
+    } else {
+      alert(`${newName} already exists`);
     }
-    else {
-      alert(`${newName} Already exist`)
-    }
+  };
 
-    
-    console.log("Form Submited", persons)
-  }
+  const handlePersonChange = (e) => setNewName(e.target.value);
+  const handlePhoneChange = (e) => setPhone(e.target.value);
+  const handleSearch = (e) => setSearch(e.target.value);
 
-  const handlePersonChange = (e) => {
-    console.log("New person", e.target.value)
-    setNewName(e.target.value)
-  }
-
-  const handlePhoneChange = (e) => {
-    console.log(e.target.value)
-    setPhone(e.target.value)
-  }
-
-  const handleSearch = (e) => {
-    setSearch(e.target.value)
-    console.log("searching", search)
-  }
   const personsToShow = persons.filter(person =>
     person.name.toLowerCase().includes(search.toLowerCase())
-  )
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Form addPerson={addPerson} handlePersonChange={handlePersonChange} handlePhoneChange={handlePhoneChange} />
+      <Form
+        addPerson={addPerson}
+        handlePersonChange={handlePersonChange}
+        handlePhoneChange={handlePhoneChange}
+      />
       <br />
       <Filter handleSearch={handleSearch} />
       <h2>Numbers</h2>
-      <div>
-        <Persons personsToShow={personsToShow} />
-      </div>
-      ...
+      <Persons personsToShow={personsToShow} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
