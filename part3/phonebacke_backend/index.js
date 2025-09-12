66d053a01a32745d16674be9e2a3bbfd1b748c60
HaXrr <hazratabubakar91@gmail.com>
2025-09-11 22:09:02 -0700

    ex:3.2 implemented info page for all the person count witj time stamp

--- part3/phonebacke_backend/index.js
+++ part3/phonebacke_backend/index.js
@@ -17,6 +17,17 @@ app.get('/api/persons', (req, res) => {
 })
 
 
+app.get('/api/info', (req, res)=>{
+    const count = persons.length
+    const time = new Date()
+    res.send(`
+        <p>Phonebook has info for ${count} people</p>
+    <p>${time}</p>
+        `)
+
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
