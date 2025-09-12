f36bcb7e40ec8304ed2773ce9ce353abfaa54162
HaXrr <hazratabubakar91@gmail.com>
2025-09-11 23:24:17 -0700

    ex:3.4 implemented api endpoint for deleting a single person by id

--- part3/phonebacke_backend/index.js
+++ part3/phonebacke_backend/index.js
@@ -4,10 +4,10 @@ const app = express()
 app.use(express.json()) // for parsing JSON bodies
 
 let persons = [
-  { id: "1", name: "Arto Hellas", number: "040-123456" },
-  { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
-  { id: "3", name: "Dan Abramov", number: "12-43-234345" },
-  { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
+    { id: "1", name: "Arto Hellas", number: "040-123456" },
+    { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
+    { id: "3", name: "Dan Abramov", number: "12-43-234345" },
+    { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
 ]
 
 
@@ -17,14 +17,19 @@ app.get('/api/persons', (req, res) => {
 })
 
 
-app.get('/api/persons/:id', (req, res)=> {
+app.get('/api/persons/:id', (req, res) => {
     const id = req.params.id
-    const person = persons.find(p=> p.id === id)
+    const person = persons.find(p => p.id === id)
     if (person) res.json(person)
-    else res.status(404).json({error: "not found"})
+    else res.status(404).json({ error: "not found" })
 })
 
-app.get('/api/info', (req, res)=>{
+app.delete('/api/persons/:id', (req, res) => {
+    persons = persons.filter(p => p.id !== req.params.id)
+    res.status(204).json({ error: "person not found" })
+})
+
+app.get('/api/info', (req, res) => {
     const count = persons.length
     const time = new Date()
     res.send(`
@@ -37,5 +42,5 @@ app.get('/api/info', (req, res)=>{
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
