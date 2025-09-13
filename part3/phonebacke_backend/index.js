db3b96f7c10c94186d87e172bc882cc12b45fb60
HaXrr <hazratabubakar91@gmail.com>
2025-09-13 01:43:18 -0700

    THere was a mistak in root route for displaying the static frontend page, so i had to remove /* from the rout and replace it with / now its resolved

--- part3/phonebacke_backend/index.js
+++ part3/phonebacke_backend/index.js
@@ -62,9 +62,10 @@ app.get('/api/info', (req, res) => {
   `)
 })
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve('dist', 'index.html'))
 })
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
