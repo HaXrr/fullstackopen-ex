2781d2ca7f2acc6adf468b37305097576cdbbd24
HaXrr <hazratabubakar91@gmail.com>
2025-09-13 01:24:58 -0700

    PhoneBook frontend and Backend work locally, than deployed backend to railway and than tried to run frontend locally with railway backend, and its working

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,5 @@
+import React,{useState} from 'react'
+
 const App = () => {
     const course = {
     name: 'Half Stack application development',
@@ -16,7 +18,22 @@ const App = () => {
       }
     ]
   }
+  const [counter,setCounter] = useState(0)
+console.log('rendering with counter value', counter)
 
+const handleIncrement = ()=>{
+    console.log('clicked the button')
+    setCounter(counter + 1) //this is not working as expected
+} 
+
+const handleDecrement = ()=>{
+  console.log('decrementing the button')
+  setCounter(counter - 1)
+}
+const handleZero = ()=>{
+  console.log('resetting the button')
+  setCounter(0)
+}
   return (
     <div>
       <Header name={course.name} />
@@ -26,12 +43,22 @@ const App = () => {
       />
       
       <Total total={ course.parts[0].exercises + course.parts[1].exercises+ course.parts[2].exercises}/>
+      <p>{counter}</p>
+      <Button handleClick={handleDecrement} text='minus' />
+      <Button handleClick={handleIncrement} text='plus' />
+      <Button handleClick={handleZero} text='zero' />
      
     </div>
   )
 }
 
 export default App
+
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
 
 const Header = (props)=>{
     return(
--- part2/phonebook/src/services/personService.js
+++ part2/phonebook/src/services/personService.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "https://fullstackopen-ex-production.up.railway.app/api/persons";
 
 
 const getAll = () => {
